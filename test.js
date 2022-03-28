e303d9698ca012cb072f4464f02be871d43dcb7d
Waaatanuki <watanukipublic@gmail.com>
2022-03-28 20:50:10 +0800

    update

--- test.js
+++
@@ -1,8 +0,0 @@
-const generateRandom = function (row, column) {
-    const seed = Math.floor(Math.random() * 100000);
-    const randomBar = seed => "0." + Math.sin(seed).toString().substring(6);
-    const xy = Math.floor(randomBar(seed) * row * column) + 1;
-    console.log(xy);
-};
-
-generateRandom(10, 10);
